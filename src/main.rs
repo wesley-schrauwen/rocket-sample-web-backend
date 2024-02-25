6f74d37a03cc23f19f830899fefd5e9b8f0c2f78
Wesley Schrauwen <>
2024-02-25 21:08:36 +0200

    moved routes into own mod

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,19 @@
 mod models;
+mod routes;
 
 #[macro_use] extern crate rocket;
 
 use std::str::FromStr;
 
-use rocket::response::{Responder, status};
-use rocket::{State};
+use rocket::response::{Responder};
 use rocket::request::FromParam;
 
-use rocket::serde::json::{Json};
-use serde::{Deserialize, Serialize};
-
-use sqlx::{FromRow, PgPool, Row};
 use sqlx::postgres::{PgPoolOptions};
-use uuid::Uuid;
 use rocket::figment::providers::{Format};
-
-use models::users::UserRecord;
-use crate::models::errors::ErrorResponse;
-use crate::models::users::{DatabaseModel, UserDTO};
-
-#[put("/person/<id>", format="json", data="<person>")]
-async fn update_person(id: Uuid, person: Json<UserDTO>, pool: &State<PgPool>) -> Result<UserRecord, ErrorResponse> {
-    UserRecord::update(&id, &person.into_inner(), pool).await
-}
-
-#[delete("/person/<id>")]
-async fn delete_person_by_id(id: Uuid, pool: &State<PgPool>) -> Result<status::NoContent, ErrorResponse> {
-    match UserRecord::delete_by_id(&id, &pool.inner()).await {
-        Ok(_) => Ok(status::NoContent),
-        Err(error) => Err(error)
-    }
-}
-
-#[get("/person/<id>")]
-async fn get_person_by_id(id: Uuid, pool: &State<PgPool>) -> Result<UserRecord, ErrorResponse> {
-    UserRecord::get_by_id(&id, pool.inner()).await
-}
-
-#[post("/person", format="json", data="<person_data>")]
-async fn create_person(pool: &State<PgPool>, person_data: Json<UserDTO>) -> Result<UserRecord, ErrorResponse> {
-    let person = person_data.into_inner();
-    UserRecord::insert(&person, &pool.inner()).await
-}
+use crate::routes::person::*;
 
 #[launch]
 async fn rocket() -> _ {
-    // let cache = KeyValueStore::new();
     let database_url = "postgres://postgres:password@localhost:5432";
 
     let pool = PgPoolOptions::new()
@@ -56,7 +23,6 @@ async fn rocket() -> _ {
         .expect("Failed to init postgres");
 
     rocket::build()
-        // .manage(cache)
         .manage(pool)
         .mount("/", routes![get_person_by_id, create_person, delete_person_by_id, update_person])
 }
---
+++ src/routes/person.rs
@@ -0,0 +1,31 @@
+use rocket::response::status;
+use rocket::serde::json::Json;
+use rocket::State;
+use sqlx::PgPool;
+use uuid::Uuid;
+use crate::models::errors::ErrorResponse;
+use crate::models::users::{DatabaseModel, UserDTO, UserRecord};
+
+#[put("/person/<id>", format="json", data="<person>")]
+pub async fn update_person(id: Uuid, person: Json<UserDTO>, pool: &State<PgPool>) -> Result<UserRecord, ErrorResponse> {
+    UserRecord::update(&id, &person.into_inner(), pool).await
+}
+
+#[delete("/person/<id>")]
+pub async fn delete_person_by_id(id: Uuid, pool: &State<PgPool>) -> Result<status::NoContent, ErrorResponse> {
+    match UserRecord::delete_by_id(&id, &pool.inner()).await {
+        Ok(_) => Ok(status::NoContent),
+        Err(error) => Err(error)
+    }
+}
+
+#[get("/person/<id>")]
+pub async fn get_person_by_id(id: Uuid, pool: &State<PgPool>) -> Result<UserRecord, ErrorResponse> {
+    UserRecord::get_by_id(&id, pool.inner()).await
+}
+
+#[post("/person", format="json", data="<person_data>")]
+pub async fn create_person(pool: &State<PgPool>, person_data: Json<UserDTO>) -> Result<UserRecord, ErrorResponse> {
+    let person = person_data.into_inner();
+    UserRecord::insert(&person, &pool.inner()).await
+}
